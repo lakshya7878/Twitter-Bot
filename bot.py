4959ed949b64c3187f5f7237b43eb98b9cb2481f
Lakshya Sharma <60431358+lakshya7878@users.noreply.github.com>
2021-09-29 00:38:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import tweepy
 
-ckey = 'ojnv1gy3V9azhBcAVCqEskZ55'
-csecret = 'NKTDcGoeAYp2Lpp8xQKi4WJIGAwK0WjxNQtWVh009asVMktroH'
-access_token = '1440308563537104905-5IegzSAYBEdEEAY7cvX8CK0RxmZiC4'
-access_secret_token = '0itPfBAWF5x601HOEFmvNP4vsdZNTUvyY3rmK1NpqULit'
+ckey = '***************'
+csecret = '*********'
+access_token = '***********'
+access_secret_token = '************'
 
 authorization = tweepy.OAuthHandler(ckey,csecret)
 authorization.set_access_token(access_token,access_secret_token)
